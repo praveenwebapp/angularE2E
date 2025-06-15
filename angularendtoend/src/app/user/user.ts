ab8a05be3885223c56b474ff93f45a84e551bc5d
PRINCE <praveenmaddela@MacBookPro.fritz.box>
2025-06-15 16:06:00 +0200

    changes related Liftcycle hooks

--- angularendtoend/src/app/user/user.ts
+++ angularendtoend/src/app/user/user.ts
@@ -1,22 +1,71 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
-  styleUrl: './user.scss'
+  styleUrl: './user.scss',
 })
-export class User {
-  constructor(private router : Router){
-    
+export class User
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  constructor(private router: Router) {
+    console.log('Constructor called.');
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    // use for components input property
+    console.log('1. ngOnChanges called.');
+  }
+  ngOnInit(): void {
+    // use for API call
+    console.log('2. ngOnInit called');
+  }
+  ngDoCheck(): void {
+    // use for every change detection
+    console.log('3. ngDoCheck called');
+  }
+  ngAfterContentInit(): void {
+    // use for ng-content
+    console.log('4. ngAfterContentInit called');
+  }
+  ngAfterContentChecked(): void {
+    console.log('5. ngAfterContentChecked called');
+  }
+  ngAfterViewInit(): void {
+    // use for viewChild
+    console.log('6. ngAfterViewInit called');
+  }
+  ngAfterViewChecked(): void {
+    console.log('7. ngAfterViewChecked called');
+  }
+  ngOnDestroy(): void {
+    console.log('8. ngOnDestroy called');
+  }
 
   loginData() {
     // user verification logic
     // this.router.navigateByUrl("structural-directive");
     this.router.navigate(['structural-directive']);
   }
-
 }
